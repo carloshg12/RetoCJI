910f5d8618282ee46a56f45c45d294224616e6e5
carloshg12 <carhergar@alu.edu.gva.es>
2024-01-30 18:26:48 +0100

    Navegaciones

--- app/src/main/java/com/example/retocji/MainActivity.kt
+++ app/src/main/java/com/example/retocji/MainActivity.kt
@@ -1,46 +1,75 @@
 package com.example.retocji
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.retocji.ui.components.NavigationBar
+import com.example.retocji.ui.components.TopBar
+import com.example.retocji.ui.components.getInfoAbout
+import com.example.retocji.ui.screens.GeneralInfo
+import com.example.retocji.ui.screens.citas
+import com.example.retocji.ui.screens.gestiones
+import com.example.retocji.ui.screens.logIn
+import com.example.retocji.ui.screens.sobreNosotros
 import com.example.retocji.ui.theme.RetoCJITheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RetoCJITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                val navController = rememberNavController()
+
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    topBar = { TopBar(navController, "I&M Asesores") },
+                    bottomBar = {
+                        NavigationBar(getInfoAbout(), navController)
+                    }
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                top = it.calculateTopPadding(),
+                                bottom = it.calculateBottomPadding()
+                            )
+                    ) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = "GeneralInfo"
+                        ) {
+                            composable("GeneralInfo") {
+                                GeneralInfo(navController)
+                            }
+                            composable("Citas") {
+                                citas()
+                            }
+                            composable("Gestiones") {
+                                gestiones()
+                            }
+                            composable("SobreNosotros") {
+                                sobreNosotros()
+                            }
+                            composable("LogIn") {
+                                logIn()
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RetoCJITheme {
-        Greeting("Android")
     }
 }
